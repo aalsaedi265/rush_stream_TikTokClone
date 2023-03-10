90e0621b30699dfc187cb4c1fe24a577f100fbc6
aalsaedi265 <aalsaedi265@gmail.com>
2023-03-10 15:34:26 -0600

    setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,34 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useState, useEffect} from 'react'
+import Navbar from '../components/Navbar'
+import Sidebar from '../components/Sidebar' 
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  // ssr -? server side rendering  runig on the BACKEND
+  const [isSSR, setIsSSr] = useState(true)
+// if running on the on the client in the react use the effect
+  useEffect(()=>{
+    //prevent server side redning whe nusing the backend
+    setIsSSr(false)
+  },[])
+  
+  if(isSSR) return null
+  //when the server is turend of show up the compneing
+  return (
+  <div >
+    <Navbar/>
+    <div className='flex gap-6 md:gap-20'>
+      <div className='h-[92vh] overflow-hidden x1:hover:overflow-auto'>
+        <Sidebar/>
+      </div>
+      <div className='mt-4 flex flex-col gap-10 overflow-auto h-[auto] videso flex-1'>
+        <Component {...pageProps} />
+      </div>
+    </div>
+  </div>
+  
+  
+  )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <>
     <h1 className="text-3xl font-bold underline">
-      Hello world!
+      ZZZZAAAWWORRRLLLDDOOOO!
     </h1>
     </>
   )
